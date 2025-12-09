c80910207432f2dd922ab320390bdf3192c36ea8
zizo ziza <zizoziza59@gmail.com>
2025-12-09 14:38:37 +0000

    fix : GET / 200 in 381ms  ○ Compiling /resume ...  ✓ Compiled /resume in

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const withMDX = createMDX({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+        "https://6000-firebase-studio-1764267609947.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev",
+        "https://9000-firebase-studio-1764267609947.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev"
+    ]
+  },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   typescript: {
     ignoreBuildErrors: true,
